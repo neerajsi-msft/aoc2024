62df17bce7bd5c59041bf7504e7d8278a610956f
Neeraj K. Singh <neerajsi@microsoft.com>
2024-12-03 21:04:57 -0800

    day4 template

---
+++ day04/src/main.rs
@@ -0,0 +1,30 @@
+use core::str;
+use std::{
+    default, env, error::Error, fs::File, io::{BufRead, BufReader, Read}, mem, ops::Mul
+};
+use thiserror::Error;
+use arrayvec::ArrayVec;
+
+#[derive(Debug, Clone, Error)]
+#[error("Command line error {0}")]
+struct CommandLineError(String);
+
+impl CommandLineError {
+    fn new(msg: &str) -> Self {
+        CommandLineError {
+            0: String::from(msg),
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut args = env::args();
+    if args.len() != 2 {
+        return Err(CommandLineError::new("Unexpected arg count.").into());
+    }
+
+    let file_name = args.nth(1).unwrap();
+
+    println!("Opening file {}", file_name);
+    Ok(())
+}
