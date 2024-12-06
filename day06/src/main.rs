c205fd27be750b3bd4267e734f22def76d7c8699
Neeraj Singh (Windows-SFS) <neerajsi@ntdev.microsoft.com>
2024-12-06 15:41:46 -0800

    day6 jumping

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -35,6 +35,161 @@ enum PuzzleError {
     ReturnedToInitialPosition,
 }
 
+
+// Note, these directions are carefully laid out, zero is up.
+// Incrementing direction represents a right turn.
+const DIRECTIONS: [(i8, i8); 4] = [
+    (-1, 0), // Up
+    (0, 1),  // Right
+    (1, 0),  // Down
+    (0, -1), // Left
+];
+
+fn change_direction(dir: usize) -> usize
+{
+    (dir + 1) % DIRECTIONS.len()
+}
+
+#[derive(Debug, Clone, Default)]
+struct ObstacleMatrix {
+    rows: Vec<Vec<usize>>,
+    cols: Vec<Vec<usize>>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Position {
+    location: (usize, usize),
+    direction: usize,
+}
+
+impl Position {
+    fn new(location: (usize, usize), direction: usize) -> Self {
+        Position{location, direction}
+    }
+}
+
+impl ObstacleMatrix {
+    fn new(rows: usize, cols: usize) -> Self {
+        let mut row_vec = Vec::new();
+        row_vec.resize_with(rows, Default::default);
+
+        let mut col_vec = Vec::new();
+        col_vec.resize_with(cols, Default::default);
+        
+        ObstacleMatrix{rows: row_vec, cols: col_vec}
+    }
+
+    fn add(&mut self, pos: (usize, usize)) {
+        let (row, col) = (&mut self.rows[pos.0], &mut self.cols[pos.1]);
+
+        let bad_insert = |dim: &[usize], val| {
+            dim.last().map_or(false, |&v| v >= val)
+        };
+
+        if bad_insert(row, pos.1) ||
+           bad_insert(col, pos.0) {
+
+            panic!("Non sorted row insert at {pos:?}");
+        }
+
+        row.push(pos.1);
+        col.push(pos.0);
+
+        ()
+    }
+
+    fn next_pos(&self, pos: &Position) -> Option<Position> {
+        let v = DIRECTIONS[pos.direction];
+
+        let (search_dim, dim_val, dim_dir) = 
+            if v.0 == 0i8 {
+                (&self.rows[pos.location.0], pos.location.1, v.1 > 0)
+            } else {
+                (&self.cols[pos.location.1], pos.location.0, v.0 > 0)
+            };
+
+        let dim_pos = search_dim.binary_search(&dim_val).expect_err("Expected not to be exactly at a obstacle");
+
+        assert!(search_dim.get(dim_pos).map_or(true, |&v| v > dim_val));
+
+        let new_dim_val =
+        if !dim_dir {
+                if dim_pos == 0 {
+                    return None;
+                }
+                
+                search_dim[dim_pos - 1] + 1
+            } else {
+                *search_dim.get(dim_pos)? - 1
+            };
+            
+        let new_dir = change_direction(pos.direction);
+        if v.0 == 0 {
+            Some(Position::new((pos.location.0, new_dim_val),new_dir))
+        } else {
+            Some(Position::new((new_dim_val, pos.location.1), new_dir))
+        }
+    }
+}
+
+fn override_next_pos(old_pos: &Position, new_pos: &Option<Position>, override_pos: &(usize, usize)) -> Option<Position>
+{
+    let v = DIRECTIONS[old_pos.direction];
+
+    let get_updated_val = |old_val, new_val, override_val, dir| -> Option<usize> {
+        match new_val {
+            None => {
+                match (old_val < override_val, dir > 0) {
+                (true, true) => Some(override_val - 1),
+                (false, false) => Some(override_val + 1),
+                _ => None
+                }
+            }
+            Some(new_val) => {
+                if old_val < override_val && override_val <= new_val {
+                    assert!(dir > 0);
+                    Some(override_val - 1)
+                } else if new_val <= override_val && override_val < old_val {
+                    assert!(dir < 0);
+                    Some(override_val + 1)
+                } else {
+                    Some(new_val)
+                }
+            }
+        }
+    };
+
+    let new_direction = change_direction(old_pos.direction);
+    if v.0 != 0 {
+        if old_pos.location.1 != override_pos.1 {
+            return *new_pos;
+        }
+
+        return Some(
+            Position::new(
+                (get_updated_val(old_pos.location.0, new_pos.map(|p| p.location.0), override_pos.0, v.0)?,
+                          override_pos.1),
+                          new_direction
+            )
+        );
+
+    } else {
+        if (old_pos.location.0 != override_pos.0) {
+            return *new_pos;
+        }
+
+        return Some(
+            Position::new(
+                (override_pos.0,
+                          get_updated_val(old_pos.location.1, new_pos.map(|p| p.location.1), override_pos.1, v.1)?),
+                          new_direction
+                )
+                        
+            
+            );
+    }
+}
+
 #[derive(Debug, Clone)]
 struct Puzzle {
     rows: usize,
@@ -42,17 +197,18 @@ struct Puzzle {
 
     guard_loc: (usize, usize),
     obstacle_set: BitSet,
+    obstacle_matrix: ObstacleMatrix,
 }
 
 impl Puzzle {
-    fn cell_index(&self, pos: (usize, usize)) -> usize {
+    fn cell_index(&self, pos: &(usize, usize)) -> usize {
         assert!(pos.0 < self.rows);
         assert!(pos.1 < self.cols);
 
         return pos.0 * self.cols + pos.1;
     }
 
-    fn is_obstacle(&self, pos: (usize, usize)) -> bool {
+    fn is_obstacle(&self, pos: &(usize, usize)) -> bool {
         self.obstacle_set.contains(self.cell_index(pos))
     }
 }
@@ -73,6 +229,7 @@ fn parse_puzzle<'a>(
 
     let mut obstacle_set = BitSet::with_capacity(cell_count);
     let mut guard_loc = None;
+    let mut obstacle_matrix = ObstacleMatrix::new(rows, cols);
 
     for i in 0..rows {
         for j in 0..cols {
@@ -80,6 +237,7 @@ fn parse_puzzle<'a>(
                 b'.' => {}
                 b'#' => {
                     obstacle_set.insert(i * cols + j);
+                    obstacle_matrix.add((i, j));
                 }
                 b'^' => {
                     if guard_loc.is_none() {
@@ -102,17 +260,10 @@ fn parse_puzzle<'a>(
         cols,
         guard_loc,
         obstacle_set,
+        obstacle_matrix,
     });
 }
 
-// Note, these directions are carefully laid out, zero is up.
-// Incrementing direction represents a right turn.
-const DIRECTIONS: [(i8, i8); 4] = [
-    (-1, 0), // Up
-    (0, 1),  // Right
-    (1, 0),  // Down
-    (0, -1), // Left
-];
 
 fn new_loc(puzzle: &Puzzle, cur_loc: &(usize, usize), direction: usize) -> Option<(usize, usize)> {
     let direction = DIRECTIONS[direction];
@@ -154,9 +305,8 @@ fn IterateThroughPuzzle<'a>(
             break;
         };
 
-        if puzzle.is_obstacle(new_loc) {
-            cur_dir += 1;
-            cur_dir %= DIRECTIONS.len();
+        if puzzle.is_obstacle(&new_loc) {
+            cur_dir = change_direction(cur_dir);
         } else {
             if !f(new_loc, cur_dir) {
                 return IterationResult::CallbackReturned;
@@ -173,37 +323,94 @@ fn IterateThroughPuzzle<'a>(
     IterationResult::Escaped
 }
 
-fn part2_checkone(
+fn part2_can_place_obstacle(
     puzzle: &Puzzle,
-    (pos): (usize, usize),
-    visited: &BitSet,
-    new_puzzle: &mut Puzzle,
-    visited_with_directions: &mut BitSet,
+    new_obstacle: &(usize, usize),
+    visited: &BitSet
 ) -> bool {
-    if pos == puzzle.guard_loc || puzzle.is_obstacle(pos) {
+    if *new_obstacle == puzzle.guard_loc || puzzle.is_obstacle(new_obstacle) {
         return false;
     }
 
     // If the cell wasn't visited in the first place, it would not be visited
     // again, so an obstacle will do nothing
-    if !visited.contains(puzzle.cell_index(pos)) {
+    if !visited.contains(puzzle.cell_index(new_obstacle)) {
+        return false;
+    }
+    true
+}
+
+fn part2_checkone(
+    puzzle: &Puzzle,
+    new_obstacle: &(usize, usize),
+    visited: &BitSet,
+    new_puzzle: &mut Puzzle,
+    visited_with_directions: &mut BitSet,
+) -> bool {
+    if !part2_can_place_obstacle(puzzle, new_obstacle, visited) {
+        return false;
+    }
+
+    visited_with_directions.clear();
+    let mut last_dir = 1usize;
+
+    //println!("Check:{:?}", new_obstacle);
+
+    // Keep going until we visit the same square facing the same direction
+    // (i.e. until insert returns false, which means the item is already in the set.)
+    let mut mark_visited_with_directions =
+        |pos, dir| {
+            if dir != last_dir {
+                //println!("\tturn:{:?}", (pos, dir));
+                last_dir = dir;
+            }
+            visited_with_directions.insert(puzzle.cell_index(&pos) * DIRECTIONS.len() + dir)
+        };
+
+    assert!(new_puzzle.obstacle_set.insert(puzzle.cell_index(new_obstacle)));
+
+    let res = IterateThroughPuzzle(&new_puzzle, &mut mark_visited_with_directions);
+
+    assert!(new_puzzle.obstacle_set.remove(puzzle.cell_index(new_obstacle)));
+
+    //println!("\tresult:{res:?}");
+
+    res != IterationResult::Escaped
+}
+
+fn part2_checkone_jumping(
+    puzzle: &Puzzle,
+    new_obstacle: &(usize, usize),
+    visited: &BitSet,
+    visited_with_directions: &mut BitSet,    
+    ) -> bool
+{
+    if !part2_can_place_obstacle(puzzle, new_obstacle, visited) {
         return false;
     }
 
     visited_with_directions.clear();
 
-    // Keep going until we visit the same square facing the same direction
-    // (i.e. until insert returns false, which means the item is already in the set.)
-    let mut mark_visited_with_directions =
-        |pos, dir| visited_with_directions.insert(puzzle.cell_index(pos) * DIRECTIONS.len() + dir);
+    //println!("Check:{:?}", new_obstacle);
 
-    assert!(new_puzzle.obstacle_set.insert(puzzle.cell_index(pos)));
+    let mut cur_pos = Position::new(puzzle.guard_loc, 0);
+    loop {
+        //println!("\tturn:{cur_pos:?}");
+        let new_pos = puzzle.obstacle_matrix.next_pos(&cur_pos);
+        let new_pos = override_next_pos(&cur_pos, &new_pos, new_obstacle);
+        let Some(new_pos) = new_pos else {
+            //println!("\tescaped");
+            return false;
+        };
 
-    let res = IterateThroughPuzzle(&new_puzzle, &mut mark_visited_with_directions);
+        if !visited_with_directions.insert(puzzle.cell_index(&new_pos.location) * DIRECTIONS.len() + new_pos.direction) {
+            //println!("\tlooped");
+            return true;
+        }
 
-    assert!(new_puzzle.obstacle_set.remove(puzzle.cell_index(pos)));
-
-    res != IterationResult::Escaped
+        assert_ne!(new_pos, cur_pos);
+        cur_pos = new_pos;
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -223,7 +430,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut visited = BitSet::with_capacity(puzzle.obstacle_set.capacity());
     let mut mark_visited = |pos, _| {
-        visited.insert(puzzle.cell_index(pos));
+        visited.insert(puzzle.cell_index(&pos));
         true
     };
 
@@ -240,13 +447,14 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut visited_with_directions =
         BitSet::with_capacity(puzzle.rows * puzzle.cols * DIRECTIONS.len());
-    let start = std::time::Instant::now();
+
+    let mut serial_timer = stopwatch::Stopwatch::start_new();
     for i in 0..puzzle.rows {
         for j in 0..puzzle.cols {
             let pos = (i, j);
             cycle_position_count += part2_checkone(
                 &puzzle,
-                pos,
+                &pos,
                 &visited,
                 &mut new_puzzle,
                 &mut visited_with_directions,
@@ -254,11 +462,12 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    let serial_time = start.elapsed().as_secs_f32();
+    serial_timer.stop();
     
     dbg!(cycle_position_count);
     
-    let parallel_start = std::time::Instant::now();
+    let mut parallel_timer = stopwatch::Stopwatch::start_new();
+
     let par_cycle_count: usize = (0..puzzle.rows).into_par_iter().map(
         |i| -> usize {
             #[derive(Debug)]
@@ -292,7 +501,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
                         let TlsState::Initialized(data) = state else {panic!("Unexpected tls state")};
 
-                        cycle_count += part2_checkone(&puzzle, pos, &visited, &mut data.new_puzzle, &mut data.visited_with_directions) as usize;
+                        cycle_count += part2_checkone(&puzzle, &pos, &visited, &mut data.new_puzzle, &mut data.visited_with_directions) as usize;
                     }
                 )
             }
@@ -301,13 +510,34 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     ).sum();
 
-    let parallel_time = parallel_start.elapsed().as_secs_f32();
+    parallel_timer.stop();
     
     dbg!(par_cycle_count);
+    
+    let mut jumping_timer = stopwatch::Stopwatch::start_new();
+    let mut jumping_cycle_count = 0usize;
+    for i in 0..puzzle.rows {
+        for j in 0..puzzle.cols {
+            let pos = (i, j);
+            jumping_cycle_count += part2_checkone_jumping(
+                &puzzle,
+                &pos,
+                &visited,
+                &mut visited_with_directions,
+            ) as usize;
+        }
+    }
+    jumping_timer.stop();
 
+    dbg!(jumping_cycle_count);
+
+    let serial_time = serial_timer.elapsed().as_micros();
+    let parallel_time = parallel_timer.elapsed().as_micros();
+    let jumping_time = jumping_timer.elapsed().as_micros();
     dbg!(serial_time);
     dbg!(parallel_time);
     dbg!(serial_time / parallel_time);
+    dbg!(jumping_time);
 
     Ok(())
 }
