a13ed5b3a953d6e48b9cd07c0b06036ed5941561
Neeraj Singh <neerajsi@microsoft.com>
2024-12-26 23:33:17 -0500

    2023 day01

---
+++ 2023/day01/src/main.rs
@@ -0,0 +1,63 @@
+use std::{array, io::{BufRead, Read}};
+
+fn main() {
+    let mut buf = Vec::new();
+    std::io::stdin().read_to_end(&mut buf).unwrap();
+
+    let number_words = [
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine"
+    ];
+
+    let sum:[usize;2] = 
+        buf.split(|&c| c == b'\n').map(|l| { 
+            let (mut first, mut last) = (None, None);
+            let (mut firstp2, mut lastp2) = (None, None);
+            
+            for (p, &c) in l.iter().enumerate() {
+                match c {
+                    b'0'..=b'9' => {
+                        let c = (c - b'0') as usize;
+                        first.get_or_insert(c);
+                        last = Some(c);
+                        firstp2.get_or_insert(c);
+                        lastp2 = Some(c)
+                    }
+                    _ => {
+                        let nw = number_words.iter().position(
+                            |&nw| l[p..].starts_with(nw.as_bytes())
+                        );
+
+                        if let Some(n) = nw {
+                            let n = n+1;
+                            firstp2.get_or_insert(n);
+                            lastp2 = Some(n)
+    
+                        }
+                    }
+                }
+            }
+
+            let conv_n = |a: Option<usize>, b: Option<usize>| {
+                if a.is_some() {
+                    a.unwrap() * 10 + b.unwrap()
+                } else {
+                    0
+                }
+            };
+
+            [conv_n(first, last), conv_n(firstp2, lastp2)]
+        })
+        .fold([0,0], |acc, v| {
+            array::from_fn(|i| acc[i] + v[i])
+        });
+
+    dbg!(sum);
+}
